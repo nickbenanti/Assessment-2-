e9e033b3812ca933b62245833268c5b5e3870d3a
nick benanti <nickbenanti324@gmail.com>
2023-02-06 08:25:16 -0600

    updtated answers

--- cart.js
+++ cart.js
@@ -97,6 +97,11 @@ console.log(calcFinalPrice(10,3,3))
 
 /*
     TEXT ANSWER HERE
+// This customer object will contain customer id number , name, email, address, and birthdays, and ammount of times the customer has dined. Name will be a string, 
+    email will be a string aswell, adress will be a string, brirthdays will be a date, and ammoutn of times dined will be a integer.I choose 
+    these data types so we will be able to track how often the custome comes to the resturant. Birthdays will allow us to send advertisements
+    to email for cupons and other offers. Customer id will be to organize all customer information and will allow easy finding of information.
+
 
 */
 
@@ -106,3 +111,19 @@ console.log(calcFinalPrice(10,3,3))
 */
 
 //CODE HERE
+
+
+const customers = [
+    {id : 1, name : "Nick Benanti", address : "123 Road Dr., Dallas, Tx, 75001", birthday : 01/02/1998, visits : 24},
+    {id : 2, name : "Kathy Benanti", address : "321 Road Dr., Dallas, Tx, 75001", birthday : 12/02/1963, visits : 14},
+    {id : 3, name : "John Doe", address : "111 Road Dr., Dallas, Tx, 75001", birthday : 01/01/1977, visits : 44},
+    {id : 4, name : "Jesse Max", address : "1453 Road Dr., Dallas, Tx, 75001", birthday : 04/12/2001, visits : 4},
+    {id : 5, name : "Laura Shea", address : "222 Road Dr., Dallas, Tx, 75001", birthday : 12/02/1985, visits : 27}
+]
+
+
+
+
+
+
+
